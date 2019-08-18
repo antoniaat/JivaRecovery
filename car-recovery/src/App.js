0f977e6789f8deca915c63d93ba9c41916b21fb9
Antonia Atanasova <antoniyaata@gmail.com>
2019-08-18 16:01:30 +0300

    Add vehicle search section

--- car-recovery/src/App.js
+++ car-recovery/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from './components/common/header/navigation/Navbar.js'
 import './App.css';
+import VehicleSearch from './components/main/vehicle-search/VehicleSearch.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar></Navbar>
+        <VehicleSearch></VehicleSearch>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
