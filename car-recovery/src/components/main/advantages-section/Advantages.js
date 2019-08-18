a230d0dad8f404953af95c542d4fd2d9f101cf2d
Antonia Atanasova <antoniyaata@gmail.com>
2019-08-18 16:28:19 +0300

    Add advantages component

--- car-recovery/src/components/main/advantages-section/Advantages.js
+++ car-recovery/src/components/main/advantages-section/Advantages.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import tlogo from '../../../assets/tlogo.png';
+import dealership from '../../../assets/icons/dealership.png';
+import prices from '../../../assets/icons/prices.png';
+import safety from '../../../assets/icons/safety.png';
 
 // Styles and Assets
 import './Advantages.scss';
 
 function Advantages() {
     return (
-        <div className='advantages-section'>
-           addvatng
-            
+        <div className='Advantages'>
+            <article>
+                <img src={tlogo}></img>
+                <h1>Our Advantages</h1>
+            </article>
+            <article>
+                <div>
+                    <img src={dealership}></img>
+                    <h5>Largest Dealership</h5>
+                    <p>AutoImage is aliquip consequat to duis elit tam cillum dolore fugiat null pariatur excepteur sint for occaecat.</p>
+                </div>
+                <div>
+                    <img src={prices}></img>
+                    <h5>Lower Car Prices</h5>
+                    <p>AutoImage is aliquip consequat to duis elit tam cillum dolore fugiat null pariatur excepteur sint for occaecat.</p>
+                </div>
+                <div>
+                    <img src={safety}></img>
+                    <h5>Multipoint Safety</h5>
+                    <p>AutoImage is aliquip consequat to duis elit tam cillum dolore fugiat null pariatur excepteur sint for occaecat.</p>
+                </div>
+            </article>
         </div>
     );
 }
