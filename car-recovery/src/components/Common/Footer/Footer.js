f208bdea06f559b1fbda632afcdd3d1374b33f94
Antonia Atanasova <antoniyaata@gmail.com>
2019-08-21 14:15:00 +0300

    Add footer

--- car-recovery/src/components/Common/Footer/Footer.js
+++ car-recovery/src/components/Common/Footer/Footer.js
@@ -10,22 +10,35 @@ class Footer extends Component {
                 <section>
                     <article>
                         <img src={tlogo}></img>
-                        Copyright &Copyright; {new Date().getFullYear()}  WordPress Theme.
-All Rights Reserved.
+                        <p>
+                            Copyright © {new Date().getFullYear()} <strong>antoniaat</strong>.
+                        </p>
                     </article>
                     <article>
-
+                        <h6>USEFUL LINKS</h6>
+                        <a href='#'>About us</a>
+                        <a href='#'>Our Services</a>
+                        <a href='#'>Information</a>
+                        <a href='#'>Privacy Policy</a>
                     </article>
                     <article>
-
+                        <h6>OUR TERMS</h6>
+                        <a href='#'>Support</a>
+                        <a href='#'>Contacts</a>
+                        <a href='#'>Typography</a>
+                        <a href='#'>FAQ</a>
                     </article>
                     <article>
-
+                        <h6>SHOWROOM</h6>
+                        <p>UK, Auto, King St. 665087 Phone.: +7 234 949-58-83 
+                            E-mail.: info@example.com
+                            © 2019 Jiva Trade</p>
                     </article>
-
                 </section>
                 <section>
-                    Copyright &Copyright; {new Date().getFullYear()} WordPress Theme. All Rights Reserved.
+                    <p>
+                        Copyright © {new Date().getFullYear()} <strong>antoniaat</strong>. All Rights Reserved.
+                    </p>
                 </section>
             </div>
         );
