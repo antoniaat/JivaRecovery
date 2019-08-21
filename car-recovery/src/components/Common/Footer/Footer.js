fe704e0172caf64b657a6afcac28c5b9478a0392
Antonia Atanasova <antoniyaata@gmail.com>
2019-08-21 14:18:06 +0300

    Make the footer function component

--- car-recovery/src/components/Common/Footer/Footer.js
+++ car-recovery/src/components/Common/Footer/Footer.js
@@ -1,48 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import tlogo from '../../../assets/tlogo.png'
 
 import './Footer.scss';
 
-class Footer extends Component {
-    render() {
-        return (
-            <div className='Footer'>
-                <section>
-                    <article>
-                        <img src={tlogo}></img>
-                        <p>
-                            Copyright © {new Date().getFullYear()} <strong>antoniaat</strong>.
-                        </p>
-                    </article>
-                    <article>
-                        <h6>USEFUL LINKS</h6>
-                        <a href='#'>About us</a>
-                        <a href='#'>Our Services</a>
-                        <a href='#'>Information</a>
-                        <a href='#'>Privacy Policy</a>
-                    </article>
-                    <article>
-                        <h6>OUR TERMS</h6>
-                        <a href='#'>Support</a>
-                        <a href='#'>Contacts</a>
-                        <a href='#'>Typography</a>
-                        <a href='#'>FAQ</a>
-                    </article>
-                    <article>
-                        <h6>SHOWROOM</h6>
-                        <p>UK, Auto, King St. 665087 Phone.: +7 234 949-58-83 
-                            E-mail.: info@example.com
-                            © 2019 Jiva Trade</p>
-                    </article>
-                </section>
-                <section>
+function Footer() {
+    return (
+        <div className='Footer'>
+            <section>
+                <article>
+                    <img src={tlogo}></img>
                     <p>
-                        Copyright © {new Date().getFullYear()} <strong>antoniaat</strong>. All Rights Reserved.
+                        Copyright © {new Date().getFullYear()} <strong>antoniaat</strong>.
+                        </p>
+                </article>
+                <article>
+                    <h6>USEFUL LINKS</h6>
+                    <a href='#'>About us</a>
+                    <a href='#'>Our Services</a>
+                    <a href='#'>Information</a>
+                    <a href='#'>Privacy Policy</a>
+                </article>
+                <article>
+                    <h6>OUR TERMS</h6>
+                    <a href='#'>Support</a>
+                    <a href='#'>Contacts</a>
+                    <a href='#'>Typography</a>
+                    <a href='#'>FAQ</a>
+                </article>
+                <article>
+                    <h6>SHOWROOM</h6>
+                    <p>UK, Auto, King St. 665087 Phone.: +7 234 949-58-83
+                        E-mail.: info@example.com
+                            © 2019 Jiva Trade</p>
+                </article>
+            </section>
+            <section>
+                <p>
+                    Copyright © {new Date().getFullYear()} <strong>antoniaat</strong>. All Rights Reserved.
                     </p>
-                </section>
-            </div>
-        );
-    }
+            </section>
+        </div>
+    );
 }
 
 export default Footer;
