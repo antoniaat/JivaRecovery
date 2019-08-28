c53f3981ee203997617d0e347dbe3a732dd83fba
Antonia Atanasova <antoniyaata@gmail.com>
2019-08-28 18:58:12 +0300

    Add map container (google maps) in the contact page

--- car-recovery/src/components/Pages/ContactPage.js
+++ car-recovery/src/components/Pages/ContactPage.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
+import MapContainer from '../../containers/Map/MapContainer.js';
 
-const ContactPage = () => (
-    <div>
-        ContactPage
-    </div>
-);
+class ContactPage extends React.PureComponent {
+    state = {
+        isMarkerShown: false,
+    }
+
+    componentDidMount() {
+        this.delayedShowMarker()
+    }
+
+    delayedShowMarker = () => {
+        setTimeout(() => {
+            this.setState({ isMarkerShown: true })
+        }, 3000)
+    }
+
+    handleMarkerClick = () => {
+        this.setState({ isMarkerShown: false })
+        this.delayedShowMarker()
+    }
+
+    render() {
+        return (
+            <MapContainer
+                isMarkerShown={this.state.isMarkerShown}
+                onMarkerClick={this.handleMarkerClick} />
+        )
+    }
+}
 
 export default ContactPage;
