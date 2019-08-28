2a3108b565c3b3149138fa687db12a71a4401b5e
Antonia Atanasova <antoniyaata@gmail.com>
2019-08-28 20:28:35 +0300

    Add breadcrumbs into page header component

--- car-recovery/src/components/Common/Header/Page-Header/PageHeader.js
+++ car-recovery/src/components/Common/Header/Page-Header/PageHeader.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './PageHeader.scss';
+import Breadcrumbs from '../Navigation/Breadcrumbs/Breadcrumbs.js'
 
 const PageHeader = (props) => {
     return (
         <div className='PageHeader'>
             <img src={props.img}></img>
             <h1>{props.title}</h1>
+            <div className='breadcrumbs'>
+                <Breadcrumbs></Breadcrumbs>
+            </div>
         </div>
     );
 }
 
 export default PageHeader;
+
