864e724ec1b306319d3ea7fcc0f36278a974f257
Antonia Atanasova <antoniyaata@gmail.com>
2019-09-01 14:26:01 +0300

    Refactor the loginform and aboutpage components

--- Client/src/components/Forms/Login/LoginForm.js
+++ Client/src/components/Forms/Login/LoginForm.js
@@ -1,61 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './LoginForm.scss';
-import tlogo from '../../../assets/tlogo.png';
+import "./LoginForm.scss";
+import tlogo from "../../../assets/tlogo.png";
 
 class LoginForm extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            email: '',
-            password: '',
-        };
+  constructor(props) {
+    super(props);
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.state = {
+      email: "",
+      password: ""
+    };
 
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-        this.setState({
-            [name]: value
-        });
-    }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
 
-    handleSubmit(event) {
-        // TODO: 
-    }
+    this.setState({
+      [name]: value
+    });
+  }
 
-    render() {
-        return (
-            <div className='LoginForm'>
-                <img src={tlogo}></img>
-                <h1>Login</h1>
-                <p><span>-</span> Explore all avaliable vehicles <span>-</span></p>
+  handleSubmit(event) {
+    // TODO:
+  }
 
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        <input type="email" name='email' value={this.state.email} onChange={this.handleInputChange} placeholder='EMAIL' />
-                    </label>
-                    <label>
-                        <input type="password" name='password' value={this.state.password} onChange={this.handleInputChange} placeholder='PASSWORD' />
-                    </label>
-                    <label className='terms-and-conditions'>
-                        <span>I accept to the <a href='#'>Terms & Privacy Policy</a></span>
-                    </label>
-                    <input type="submit" value="LOGIN" />
-                    <label className='additional-links'>
-                        <span><a href='#'>FORGOT PASSWORD</a></span>
-                        <span><a href='#'>NEW TO JIVATRADE?</a></span>
-                    </label>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="LoginForm">
+        <img src={tlogo}></img>
+        <h1>Login</h1>
+        <p>
+          <span>-</span> Explore all avaliable vehicles <span>-</span>
+        </p>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleInputChange}
+              placeholder="EMAIL"
+            />
+          </label>
+          <label>
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              placeholder="PASSWORD"
+            />
+          </label>
+          <label className="terms-and-conditions">
+            <span>
+              I accept to the <a href="#">Terms & Privacy Policy</a>
+            </span>
+          </label>
+          <input type="submit" value="LOGIN" />
+          <label className="additional-links">
+            <span>
+              <a href="#">FORGOT PASSWORD</a>
+            </span>
+            <span>
+              <a href="#">NEW TO JIVATRADE?</a>
+            </span>
+          </label>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default LoginForm;
--- Client/src/containers/Pages/AboutPage.js
+++ Client/src/containers/Pages/AboutPage.js
@@ -1,30 +1,33 @@
-import React, { Component } from 'react';
-import About from '../../components/Common/About/About.js'
-import Brands from '../../components/Common/Brands/Brands.js';
-import PageHeader from '../../components/Common/Header/Page-Header/PageHeader.js';
+import React, { Component } from "react";
+import About from "../../components/Common/About/About.js";
+import Brands from "../../components/Common/Brands/Brands.js";
+import PageHeader from "../../components/Common/Header/Page-Header/PageHeader.js";
 
 class AboutPage extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            img: 'http://autoimage.templines.org/wp-content/uploads/2018/10/fbg.jpg',
-            title: 'About Us'
-        };
-    }
+    this.state = {
+      img: "http://autoimage.templines.org/wp-content/uploads/2018/10/fbg.jpg",
+      title: "About Us"
+    };
+  }
 
-    render() {
-        return (
-            <div className='AboutPage'>
-                <div>
-                    <PageHeader img={this.state.img} title={this.state.title}></PageHeader>
-                </div>
+  render() {
+    return (
+      <div className="AboutPage">
+        <div>
+          <PageHeader
+            img={this.state.img}
+            title={this.state.title}
+          ></PageHeader>
+        </div>
 
-                <About></About>
-                <Brands></Brands>
-            </div>
-        );
-    }
+        <About></About>
+        <Brands></Brands>
+      </div>
+    );
+  }
 }
 
 export default AboutPage;
