55070434c3fb81242bf5628480c9f2fa99f9561b
Antonia Atanasova <antoniyaata@gmail.com>
2020-03-22 12:58:37 +0200

    Refactor header slider component

--- client/src/components/Common/Header/Slider/Slide.js
+++ client/src/components/Common/Header/Slider/Slide.js
@@ -3,14 +3,10 @@ import React from "react";
 const Slide = ({ title, img, text }) => {
   return (
     <div>
-      <img src={firstSlide} alt="slide-img"></img>
-      <div className="HeaderSlider-center">
+      <img src={img} alt="slide-img"></img>
+      <div className="header-slider-center">
         <h1>{title}</h1>
-        <p className="HeaderSlider-text">
-          VehRecovery is dolor sit amet consectetur adipisicing elit. Cupiditate
-          incidunt praesentium, rerum voluptatem in reiciendis officia harum
-          repudiandae tempore suscipit ex ea, adipisci ab porro.
-        </p>
+        <p className="header-slider-text">{text}</p>
       </div>
     </div>
   );
