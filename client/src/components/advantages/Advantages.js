ddd602f9bf70fbf00c6a5908844eabdbaae6be07
Antonia Atanasova <antoniyaata@gmail.com>
2020-03-22 13:51:28 +0200

    Refactor Advantages component

--- client/src/components/advantages/Advantages.js
+++ client/src/components/advantages/Advantages.js
@@ -1,84 +1,37 @@
-import React, { Component } from "react";
-import tlogo from "../../assets/images/tlogo.png";
-import dealership from "../../assets/icons/dealership.png";
-import prices from "../../assets/icons/prices.png";
-import safety from "../../assets/icons/safety.png";
+import React from "react";
+import AdvantagesHeader from "./header/AdvantagesHeader";
 
 import "./Advantages.scss";
-import icons from "../icons/FontAwesomeIcons";
+import imageIcons from "../icons/ImageIcons";
+import Advantage from "./Advantage";
 
-class Advantages extends Component {
-  constructor(props) {
-    super(props);
-    this.firstArticleClick = this.firstArticleClick.bind(this);
-    this.secondArticleClick = this.secondArticleClick.bind(this);
-    this.thirdArticleClick = this.thirdArticleClick.bind(this);
+const Advantages = () => {
+  return (
+    <div className="advantages">
+      <AdvantagesHeader />
 
-    this.state = {
-      firstIsActive: false,
-      secondIsActive: true,
-      thirdIsActive: false
-    };
-  }
-
-  firstArticleClick() {
-    this.setState({ firstIsActive: !this.state.firstIsActive });
-  }
-  secondArticleClick() {
-    this.setState({ secondIsActive: !this.state.secondIsActive });
-  }
-  thirdArticleClick() {
-    this.setState({ thirdIsActive: !this.state.thirdIsActive });
-  }
-
-  render() {
-    return (
-      <div className="advantages">
-        <article>
-          <img src={tlogo} alt="logo"></img>
-          <h1>Our Advantages</h1>
-        </article>
-        <article>
-          <div
-            onClick={this.firstArticleClick}
-            className={this.state.firstIsActive ? "active" : "notActive"}
-          >
-            <img src={dealership} alt="dealership-img"></img>
-            <h5>Largest Dealership</h5>
-            <p>
-              AutoImage is aliquip consequat to duis elit tam cillum dolore
-              fugiat null pariatur excepteur sint for occaecat.
-            </p>
-            <button>{icons.arrowUp}</button>
-          </div>
-          <div
-            onClick={this.secondArticleClick}
-            className={this.state.secondIsActive ? "active" : "notActive"}
-          >
-            <img src={prices} alt="prices-img"></img>
-            <h5>Lower Car Prices</h5>
-            <p>
-              AutoImage is aliquip consequat to duis elit tam cillum dolore
-              fugiat null pariatur excepteur sint for occaecat.
-            </p>
-            <button>{icons.arrowUp}</button>
-          </div>
-          <div
-            onClick={this.thirdArticleClick}
-            className={this.state.thirdIsActive ? "active" : "notActive"}
-          >
-            <img src={safety} alt="safety-img"></img>
-            <h5>Multipoint Safety</h5>
-            <p>
-              AutoImage is aliquip consequat to duis elit tam cillum dolore
-              fugiat null pariatur excepteur sint for occaecat.
-            </p>
-            <button>{icons.arrowUp}</button>
-          </div>
-        </article>
-      </div>
-    );
-  }
-}
+      <article>
+        <Advantage
+          img={imageIcons.dealership}
+          title="Голяма верига"
+          text="Може да откриете най-голямата верига от доставчици на транспортни
+            услуги и пътна помощ в България."
+        />
+        <Advantage
+          img={imageIcons.prices}
+          title="Ниски цени"
+          text="Ние ви помагаме да откриете най-ниските и достъпни цени за транспортната услуга
+          от която имате нужда.."
+        />
+        <Advantage
+          img={imageIcons.safety}
+          title="Безопасност"
+          text="Всички доставчици на транспортни услуги и пътна помощ в платформата
+          гарантират честност и коректност."
+        />
+      </article>
+    </div>
+  );
+};
 
 export default Advantages;
---
+++ client/src/components/advantages/header/AdvantagesHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import tlogo from "../../../assets/images/tlogo.png";
+
+const AdvantagesHeader = () => {
+  return (
+    <article>
+      <img src={tlogo} alt="logo"></img>
+      <h1>Защо да изберете нас</h1>
+    </article>
+  );
+};
+
+export default AdvantagesHeader;
