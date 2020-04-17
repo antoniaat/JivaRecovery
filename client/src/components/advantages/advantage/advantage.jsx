9d6abc3ff456ccb02818a827ed4449aa4c41f26c
Antonia Atanasova <antoniyaata@gmail.com>
2020-04-17 17:21:11 +0300

    Add responsive styles for advantages section

--- client/src/components/advantages/advantage/advantage.jsx
+++ client/src/components/advantages/advantage/advantage.jsx
@@ -1,25 +1,17 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-
 import './advantage.scss';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { string } from 'prop-types';
 import { arrowUp } from '../../icons/FontAwesomeIcons';
 
-const Advantage = ({ img, title, text }) => {
-  const [active, setActive] = useState(true);
-  const isActiveClassName = active ? 'active' : 'not-active';
-
-  return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-    <div onClick={() => setActive(!active)} className={`advantage ${isActiveClassName}`}>
-      <img src={img} alt={`${img}-img`} />
-      <h5>{title}</h5>
-      <p>{text}</p>
-      <button type="button">{arrowUp}</button>
-    </div>
-  );
-};
+const Advantage = ({ img, title, text }) => (
+  <article className="advantage">
+    <img src={img} alt={`${img}-img`} />
+    <h5>{title}</h5>
+    <p>{text}</p>
+    <button type="button">{arrowUp}</button>
+  </article>
+);
 
 Advantage.propTypes = {
   img: string.isRequired,
