b8d4e2832625c9fc713b7e9066b319127250b02a
Antonia Atanasova <antoniyaata@gmail.com>
2020-04-17 16:16:26 +0300

    Desctructure data options

--- client/src/components/vehicle/search/dropdown-item/dropdown-item.jsx
+++ client/src/components/vehicle/search/dropdown-item/dropdown-item.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import Dropdown from 'react-dropdown';
 import { string } from 'prop-types';
 
-const DropdownItem = ({ title, data, placeholder }) => (
-  <article>
-    <h6>{title}</h6>
-    <Dropdown
-      options={data.options}
-      value={data.defaultOption}
-      placeholder={placeholder}
-    />
-  </article>
-);
+const DropdownItem = ({ title, data, placeholder }) => {
+  const { options, defaultOption } = data;
+
+  return (
+    <article>
+      <h6>{title}</h6>
+      <Dropdown
+        options={options}
+        value={defaultOption}
+        placeholder={placeholder}
+      />
+    </article>
+  );
+};
 
 DropdownItem.propTypes = {
   title: string.isRequired,
