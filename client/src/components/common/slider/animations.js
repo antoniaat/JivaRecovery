a28c5822a840c4f3474f82c2ad331eb76b96b168
Antonia Atanasova <antoniyaata@gmail.com>
2020-04-17 16:01:54 +0300

    Refactor slider components

--- client/src/components/common/slider/animations.js
+++ client/src/components/common/slider/animations.js
@@ -1,19 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var text = document.getElementsByClassName("HeaderSlider-text")[0];
-  var newDom = "";
-  var animationDelay = 6;
+document.addEventListener('DOMContentLoaded', () => {
+  const text = document.getElementsByClassName('HeaderSlider-text')[0];
+  let newDom = '';
+  const animationDelay = 6;
 
   for (let i = 0; i < text.innerText.length; i++) {
-    newDom +=
-      '<span class="char">' +
-      (text.innerText[i] == " " ? "&nbsp;" : text.innerText[i]) +
-      "</span>";
+    newDom
+      += `<span class="char">${
+        text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i]
+      }</span>`;
   }
 
   text.innerHTML = newDom;
-  var length = text.children.length;
+  const { length } = text.children;
 
   for (let i = 0; i < length; i++) {
-    text.children[i].style["animation-delay"] = animationDelay * i + "ms";
+    text.children[i].style['animation-delay'] = `${animationDelay * i}ms`;
   }
-}); 
+});
--- client/src/components/common/slider/slider.jsx
+++ client/src/components/common/slider/slider.jsx
@@ -1,52 +1,44 @@
+import firstSlide from 'assets/images/header-slider/1.jpg';
+import secondSlide from 'assets/images/header-slider/2.jpg';
+
 import './slider.scss';
 
 import React from 'react';
 import Slider from 'react-slick';
-import firstSlide from 'assets/images/header-slider/1.jpg';
-import secondSlide from 'assets/images/header-slider/2.jpg';
-
 import Slide from './slide';
 
-const HeaderSlider = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    lazyLoad: true,
-    adaptiveHeight: true,
-    fade: true,
-    arrows: true,
-    autoplaySpeed: 5000,
-  };
-
-  return (
-    <Slider {...settings} className="header-slider">
-      <Slide
-        title="VehRecovery"
-        img={firstSlide}
-        text=" Това е платформа, която помага на всички хора нуждаещи се от пътна
-            помощ или различни транспортни услуги. Тук можете да откриете
-            най-добрите и надеждни доставчици на услугите от които се нуждаете."
-      />
-      <Slide
-        title="VehRecovery"
-        img={secondSlide}
-        text=" Това е платформа, която помага на всички хора нуждаещи се от пътна
-            помощ или различни транспортни услуги. Тук можете да откриете
-            най-добрите и надеждни доставчици на услугите от които се нуждаете."
-      />
-      <Slide
-        title="VehRecovery"
-        img={firstSlide}
-        text=" Това е платформа, която помага на всички хора нуждаещи се от пътна
-          помощ или различни транспортни услуги. Тук можете да откриете
-          най-добрите и надеждни доставчици на услугите от които се нуждаете."
-      />
-    </Slider>
-  );
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  lazyLoad: true,
+  adaptiveHeight: true,
+  fade: true,
+  arrows: true,
+  autoplaySpeed: 5000,
 };
+
+const slidesData = [
+  {
+    img: firstSlide,
+  },
+  {
+    img: secondSlide,
+  },
+  {
+    img: firstSlide,
+  },
+];
+
+const HeaderSlider = () => (
+  <Slider {...settings} className="header-slider">
+    {slidesData.map(({ title, img, text }) => (
+      <Slide title={title} img={img} text={text} />
+    ))}
+  </Slider>
+);
 
 export default HeaderSlider;
