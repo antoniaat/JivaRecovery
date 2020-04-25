a9d00121a6d963bdc7aec729d15158f3dd4485e7
Antonia Atanasova <antoniyaata@gmail.com>
2020-04-25 11:47:11 +0300

    Refactor partners component

--- client/src/components/partners/partners.jsx
+++ client/src/components/partners/partners.jsx
@@ -4,16 +4,14 @@ import './partners.scss';
 
 import React from 'react';
 
-function Partners() {
-  return (
-    <div className="partners">
-      <h2>Партньори</h2>
-      <span>-</span>
-      <p>КОМПАНИИТЕ, КОИТО НИ СЕ ДОВЕРИХА</p>
-      <span>-</span>
-      <img src={brands} alt="brand-img" />
-    </div>
-  );
-}
+const Partners = () => (
+  <div className="partners">
+    <h2 className="partners-title">Партньори</h2>
+    <span className="partners-line">-</span>
+    <p className="partners-subtitle">КОМПАНИИТЕ, КОИТО НИ СЕ ДОВЕРИХА</p>
+    <span className="partners-line">-</span>
+    <img src={brands} alt="brand-img" />
+  </div>
+);
 
 export default Partners;
