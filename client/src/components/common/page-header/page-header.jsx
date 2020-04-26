c01b38946f11276d7f091f2aeddcd359a2053408
Antonia Atanasova <antoniyaata@gmail.com>
2020-04-26 18:21:04 +0300

    Add subtitle in page header component

--- client/src/components/common/page-header/page-header.jsx
+++ client/src/components/common/page-header/page-header.jsx
@@ -3,14 +3,22 @@ import './page-header.scss';
 import React from 'react';
 import { string } from 'prop-types';
 
-const PageHeader = ({ title }) => (
+const PageHeader = ({ title, subtitle }) => (
   <div className="page-header">
     <h1 className="page-header-title">{title}</h1>
+    {subtitle
+      ? <h3 className="page-header-subtitle">{subtitle}</h3>
+      : null}
   </div>
 );
 
+PageHeader.defaultProps = {
+  subtitle: '',
+};
+
 PageHeader.propTypes = {
   title: string.isRequired,
+  subtitle: string,
 };
 
 export default PageHeader;
