8c4609fa5e3116aab99e157f85affe917b01420d
Antonia Atanasova <antoniyaata@gmail.com>
2020-05-24 13:06:25 +0300

    Add edit btn in request expanded row

--- client/src/components/administration/request/request.jsx
+++ client/src/components/administration/request/request.jsx
@@ -8,24 +8,29 @@ const Request = ({
   pickupDate,
   deliveryDate,
 }) => (
-  <section className="request">
-    <article className="request-pickup-location">
-      <h3 className="request-heading">Място за взимане:</h3>
-      <span className="request-content">{pickupLocation}</span>
-    </article>
-    <article className="request-delivery-location">
-      <h3 className="request-heading">Място за доставяне:</h3>
-      <span className="request-content">{deliveryLocation}</span>
-    </article>
-    <article className="request-pickup-date">
-      <h3 className="request-heading">Дата за взимане:</h3>
-      <span className="request-content">{pickupDate}</span>
-    </article>
-    <article className="request-delivery-date">
-      <h3 className="request-heading">Дата за доставяне:</h3>
-      <span className="request-content">{deliveryDate}</span>
-    </article>
-  </section>
+  <>
+    <div className="request">
+      <section className="request-main">
+        <article>
+          <h3 className="request-heading">Място за взимане:</h3>
+          <span className="request-content">{pickupLocation}</span>
+        </article>
+        <article>
+          <h3 className="request-heading">Място за доставяне:</h3>
+          <span className="request-content">{deliveryLocation}</span>
+        </article>
+        <article>
+          <h3 className="request-heading">Дата за взимане:</h3>
+          <span className="request-content">{pickupDate}</span>
+        </article>
+        <article>
+          <h3 className="request-heading">Дата за доставяне:</h3>
+          <span className="request-content">{deliveryDate}</span>
+        </article>
+      </section>
+      <button className="button button-primary request-btn">Редактирай</button>
+    </div>
+  </>
 );
 
 export default Request;
