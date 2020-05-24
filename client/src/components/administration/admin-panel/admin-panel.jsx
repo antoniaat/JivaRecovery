7b41429d0c664e7a8fcc3f2b0bf33438e3c23274
Antonia Atanasova <antoniyaata@gmail.com>
2020-05-24 12:07:27 +0300

    Add react data table component for requests in admin panel

--- client/src/components/administration/admin-panel/admin-panel.jsx
+++ client/src/components/administration/admin-panel/admin-panel.jsx
@@ -3,6 +3,7 @@ import "react-tabs/style/react-tabs.css";
 
 import React, { useState, useEffect } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import DataTable from "react-data-table-component";
 import Request from "../request/request";
 import requestService from "../../../services/request-service";
 
@@ -10,47 +11,85 @@ const AdminPanel = () => {
   let [requests, setRequests] = useState([]);
   const requestColumns = [
     {
-      name: 'Марка',
-      selector: 'brand',
+      name: "Марка",
+      selector: "brand",
       sortable: true,
     },
     {
-      name: 'Модел',
-      selector: 'model',
+      name: "Модел",
+      selector: "model",
       sortable: true,
     },
     {
-      name: 'Година',
-      selector: 'year',
+      name: "Година",
+      selector: "year",
       sortable: true,
     },
+    // {
+    //   name: "Взимане",
+    //   selector: "pickupLocation",
+    //   sortable: true,
+    // },
+    // {
+    //   name: "Доставяне",
+    //   selector: "deliveryLocation",
+    //   sortable: true,
+    // },
+    // {
+    //   name: "Дата - Взимане",
+    //   selector: "pickupDate",
+    //   sortable: true,
+    // },
+    // {
+    //   name: "Дата - Доставяне",
+    //   selector: "deliveryDate",
+    //   sortable: true,
+    // },
     {
-      name: 'Местоположение за взимане',
-      selector: 'pickupLocation',
-      sortable: true,
-    },
-    {
-      name: 'Местоположение за доставяне',
-      selector: 'deliveryLocation',
-      sortable: true,
-    },
-    {
-      name: 'Дата за взимане',
-      selector: 'pickupDate',
-      sortable: true,
-    },
-    {
-      name: 'Дата за доставяне',
-      selector: 'deliveryDate',
-      sortable: true,
-    },
-    {
-      name: 'Състояние',
-      selector: 'condition',
+      name: "Състояние",
+      selector: "condition",
       sortable: true,
     },
   ];
-  
+
+  const ExpanableRow = ({ data }) => {
+    const { pickupLocation, deliveryLocation, pickupDate, deliveryDate } = data;
+
+    return (
+      <Request
+        pickupLocation={pickupLocation}
+        deliveryLocation={deliveryLocation}
+        pickupDate={pickupDate}
+        deliveryDate={deliveryDate}
+      />
+    );
+  };
+
+  // const requestData = requests.map(
+  //   ({
+  //     brand,
+  //     model,
+  //     year,
+  //     pickupLocation,
+  //     deliveryLocation,
+  //     pickupDate,
+  //     deliveryDate,
+  //     condition,
+  //   }) => {
+  //     return (
+  //       <Request
+  //         brand={brand}
+  //         model={model}
+  //         year={year}
+  //         pickupLocation={pickupLocation}
+  //         deliveryLocation={deliveryLocation}
+  //         pickupDate={pickupDate}
+  //         deliveryDate={deliveryDate}
+  //         condition={condition}
+  //       />
+  //     );
+  //   }
+  // );
 
   useEffect(() => {
     requestService.getRequest("all").then((res) => {
@@ -68,33 +107,18 @@ const AdminPanel = () => {
         </TabList>
 
         <TabPanel>
-          {requests.length > 0
-            ? requests.map(
-              ({
-                brand,
-                model,
-                year,
-                pickupLocation,
-                deliveryLocation,
-                pickupDate,
-                deliveryDate,
-                condition,
-              }) => {
-                return (
-                  <Request
-                    brand={brand}
-                    model={model}
-                    year={year}
-                    pickupLocation={pickupLocation}
-                    deliveryLocation={deliveryLocation}
-                    pickupDate={pickupDate}
-                    deliveryDate={deliveryDate}
-                    condition={condition}
-                  />
-                );
-              }
-            )
-            : "Няма текущи заявки"}
+          {requests.length > 0 ? (
+            <DataTable
+              title="Requests"
+              columns={requestColumns}
+              data={requests}
+              expandableRows
+              expandableRowDisabled={(row) => row.disabled}
+              expandableRowsComponent={<ExpanableRow />}
+            />
+          ) : (
+            "Няма текущи заявки"
+          )}
         </TabPanel>
         <TabPanel>
           <p>Users</p>
