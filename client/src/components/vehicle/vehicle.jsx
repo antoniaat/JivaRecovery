832e4d8c974614a97761b78226588d90604da718
Antonia Atanasova <antoniyaata@gmail.com>
2020-05-25 12:34:21 +0300

    Add animation to vehicle component

--- client/src/components/vehicle/vehicle.jsx
+++ client/src/components/vehicle/vehicle.jsx
@@ -1,28 +1,37 @@
-import './vehicle.scss';
+import "./vehicle.scss";
 
-import React from 'react';
-import { string, boolean, number } from 'prop-types';
-import { calendar, road, transmission } from '../icons/font-awesome-icons';
-import VehicleSpecification from './specification/vehicle-specification';
+import React from "react";
+import Fade from "react-reveal/Fade";
+import { string, boolean, number } from "prop-types";
+import { calendar, road, transmission } from "../icons/font-awesome-icons";
+import VehicleSpecification from "./specification/vehicle-specification";
 
 const Vehicle = ({
-  name, price, year, mileage, vehicleTransmission, isNew, img,
+  name,
+  price,
+  year,
+  mileage,
+  vehicleTransmission,
+  isNew,
+  img,
 }) => (
-  <div className="vehicle">
-    <div className={isNew ? 'vehicle-rental' : 'hidden'}>НОВО</div>
-    <img src={img} alt="vehicle-img" />
-    <p className="vehicle-price">{price}</p>
-    <h5 className="vehicle-name">{name}</h5>
-    <div className="vehicle-specifications">
-      <VehicleSpecification icon={calendar} text={year} />
-      <VehicleSpecification icon={road} text={mileage} />
-      <VehicleSpecification icon={transmission} text={vehicleTransmission} />
+  <Fade left>
+    <div className="vehicle">
+      <div className={isNew ? "vehicle-rental" : "hidden"}>НОВО</div>
+      <img src={img} alt="vehicle-img" />
+      <p className="vehicle-price">{price}</p>
+      <h5 className="vehicle-name">{name}</h5>
+      <div className="vehicle-specifications">
+        <VehicleSpecification icon={calendar} text={year} />
+        <VehicleSpecification icon={road} text={mileage} />
+        <VehicleSpecification icon={transmission} text={vehicleTransmission} />
+      </div>
     </div>
-  </div>
+  </Fade>
 );
 
 Vehicle.defaultProps = {
-  vehicleTransmission: 'Manual',
+  vehicleTransmission: "Manual",
   rental: true,
 };
 
