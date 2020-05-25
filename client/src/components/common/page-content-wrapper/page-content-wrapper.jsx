914e5048cc8694ea296632d3fdef17619dc54302
Antonia Atanasova <antoniyaata@gmail.com>
2020-05-25 10:34:00 +0300

    Add page content wrapper

---
+++ client/src/components/common/page-content-wrapper/page-content-wrapper.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { string, element, oneOfType, arrayOf } from "prop-types";
+
+const PageContentWrapper = ({ children, className }) => (
+  <div className={`page-content-wrapper ${className}`}>{children}</div>
+);
+
+PageContentWrapper.propTypes = {
+  className: string,
+  children: oneOfType([element, arrayOf(element)]).isRequired,
+};
+
+PageContentWrapper.defaultProps = { className: "" };
+
+export default PageContentWrapper;
