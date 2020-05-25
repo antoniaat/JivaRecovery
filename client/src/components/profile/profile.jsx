390bf1579101f86515c7760bcfcdd60ff86ef7b0
Antonia Atanasova <antoniyaata@gmail.com>
2020-05-25 17:50:32 +0300

    Move profile data in separate component

---
+++ client/src/components/profile/profile.jsx
@@ -0,0 +1,42 @@
+import React, { useContext, useEffect } from "react";
+
+import userService from "../../services/user-service";
+import { AuthContext } from "../../ContextWrapper";
+
+const Profile = () => {
+  const { auth } = useContext(AuthContext);
+  const [state, setState] = React.useState({
+    name: "",
+    email: "",
+    phone: "",
+    requests: [],
+    feedback: [],
+  });
+
+  useEffect(() => {
+    userService.getUser(auth).then((res) => {
+      const { name, email, phone, requests, feedback } = res;
+
+      setState({
+        name: name,
+        email: email,
+        phone: phone,
+        requests: requests,
+        feedback: feedback,
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      <div>Profile</div>
+      <div>{state.name}</div>
+      <div>{state.email}</div>
+      <div>{state.phone}</div>
+      <div>{state.requests}</div>
+      <div>{state.feedback}</div>
+    </>
+  );
+};
+
+export default Profile;
--- client/src/containers/pages/profile-page.jsx
+++ client/src/containers/pages/profile-page.jsx
@@ -1,45 +1,15 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import TopBar from "components/common/header/topbar/top-bar";
 import Navbar from "components/common/header/navigation/navbar";
 import Footer from "components/common/footer/footer";
-
-import userService from "../../services/user-service";
-import { AuthContext } from "../../ContextWrapper";
+import Profile from "components/profile/profile";
 
 const ProfilePage = () => {
-  const { auth } = useContext(AuthContext);
-  const [state, setState] = React.useState({
-    name: "",
-    email: "",
-    phone: "",
-    requests: [],
-    feedback: [],
-  });
-
-  useEffect(() => {
-    userService.getUser(auth).then((res) => {
-      const { name, email, phone, requests, feedback } = res;
-
-      setState({
-        name: name,
-        email: email,
-        phone: phone,
-        requests: requests,
-        feedback: feedback,
-      });
-    });
-  }, []);
-
   return (
     <>
       <TopBar />
       <Navbar />
-      <div>Profile</div>
-      <div>{state.name}</div>
-      <div>{state.email}</div>
-      <div>{state.phone}</div>
-      <div>{state.requests}</div>
-      <div>{state.feedback}</div>
+      <Profile />
       <Footer />
     </>
   );
