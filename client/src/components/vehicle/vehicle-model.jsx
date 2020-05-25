317a19e5f87f1dfda915a8e5fd1b4fc0a3dae846
Antonia Atanasova <antoniyaata@gmail.com>
2020-05-25 12:35:41 +0300

    Add animation to vehicle model component

--- client/src/components/vehicle/vehicle-model.jsx
+++ client/src/components/vehicle/vehicle-model.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+
 import { string, number } from "prop-types";
 
 import "./vehicle-model.scss";
 
 const VehicleModel = ({ img, name, offersCount, lowestPriceOffer }) => (
-  <div className="vehicle-model">
-    <div>
-      <img src={img} alt={name} />
-      <h6>{name}</h6>
-      <p>
-        {offersCount} оферти стартиращи от {lowestPriceOffer} лева
-      </p>
+  <Fade left>
+    <div className="vehicle-model">
+      <div>
+        <img src={img} alt={name} />
+        <h6>{name}</h6>
+        <p>
+          {offersCount} оферти стартиращи от {lowestPriceOffer} лева
+        </p>
+      </div>
     </div>
-  </div>
+  </Fade>
 );
 
 VehicleModel.defaultProps = {
