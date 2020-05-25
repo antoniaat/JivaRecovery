7061d51be6199e95dc28be86a310a6f4700036e0
Antonia Atanasova <antoniyaata@gmail.com>
2020-05-25 17:29:11 +0300

    Redesign form header component

--- client/src/components/forms/form-header/form-header.jsx
+++ client/src/components/forms/form-header/form-header.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import truckImg from "assets/icons/mover-truck.png";
 
-import tlogo from 'assets/images/tlogo.png';
+import "./form-header.scss";
+
+import React from "react";
+import Slide from "react-reveal/Slide";
 
 const FormHeader = ({ title, subtitle }) => (
-  <>
-    <img src={tlogo} alt="logo" />
+  <section className="form-header">
+    <Slide left>
+      <div className="form-header-image-wrapper">
+        <img src={truckImg} alt="logo" />
+      </div>
+    </Slide>
     <h1>{title}</h1>
     <p>
-      <span>-</span>
-      {' '}
-      {subtitle}
-      {' '}
-      <span>-</span>
+      <span>-</span> {subtitle} <span>-</span>
     </p>
-  </>
+  </section>
 );
 
 export default FormHeader;
