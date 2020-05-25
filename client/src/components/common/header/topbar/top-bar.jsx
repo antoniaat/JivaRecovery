c998e4114250f0a86525b70e08bc824698d24783
Antonia Atanasova <antoniyaata@gmail.com>
2020-05-25 10:40:51 +0300

    Wrap the topbar in page content wrapper

--- client/src/components/common/header/topbar/top-bar.jsx
+++ client/src/components/common/header/topbar/top-bar.jsx
@@ -5,10 +5,11 @@ import React from "react";
 import { phoneIcon, mailIcon } from "../../../icons/font-awesome-icons";
 import SocialIcons from "./social-icons/social-icons";
 import ContactBox from "./contact-box/contact-box";
+import PageContentWrapper from "components/common/page-content-wrapper/page-content-wrapper";
 
 const TopBar = () => (
   <div className="top-bar">
-    <section className="top-bar-wrapper">
+    <PageContentWrapper className="top-bar-wrapper">
       <section className="contacts-container">
         <ContactBox icon={phoneIcon} text="ТЕЛЕФОН: +7957 511350" />
         <ContactBox
@@ -18,7 +19,7 @@ JIVATRADEUK@GMAIL.COM"
         />
       </section>
       <SocialIcons />
-    </section>
+    </PageContentWrapper>
   </div>
 );
 
--- client/src/components/common/page-content-wrapper/page-content-wrapper.jsx
+++ client/src/components/common/page-content-wrapper/page-content-wrapper.jsx
@@ -1,3 +1,5 @@
+import '../page-content-wrapper/page-content-wrapper.scss';
+
 import React from "react";
 import { string, element, oneOfType, arrayOf } from "prop-types";
 
