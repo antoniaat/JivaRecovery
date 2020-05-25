8913874e13b07977bd4bad0b17ec26a64d1cc884
Antonia Atanasova <antoniyaata@gmail.com>
2020-05-25 12:36:53 +0300

    Add animation to benefit component

--- client/src/components/benefits/benefit.jsx
+++ client/src/components/benefits/benefit.jsx
@@ -1,18 +1,21 @@
-import './benefit.scss';
+import "./benefit.scss";
 
-import React from 'react';
-import { string } from 'prop-types';
+import React from "react";
+import Flip from 'react-reveal/Flip';
+import { string } from "prop-types";
 
 const Benefit = ({ icon, title, subtitle }) => (
-  <div className="benefit">
-    <div className="benefit-icon-wrapper">
-      <span>{icon}</span>
+  <Flip top>
+    <div className="benefit">
+      <div className="benefit-icon-wrapper">
+        <span>{icon}</span>
+      </div>
+      <div className="benefit-content-box">
+        <p className="benefit-content-box-title">{title}</p>
+        <p className="benefit-content-box-subtitle">{subtitle}</p>
+      </div>
     </div>
-    <div className="benefit-content-box">
-      <p className="benefit-content-box-title">{title}</p>
-      <p className="benefit-content-box-subtitle">{subtitle}</p>
-    </div>
-  </div>
+  </Flip>
 );
 
 Benefit.propTypes = {
