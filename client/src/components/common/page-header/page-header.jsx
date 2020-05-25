cd8ee32f1446873720531d66f6ddc000e945b844
Antonia Atanasova <antoniyaata@gmail.com>
2020-05-25 11:18:43 +0300

    Add animations to header slider, remove animations.scss

--- client/src/components/common/page-header/page-header.jsx
+++ client/src/components/common/page-header/page-header.jsx
@@ -1,19 +1,17 @@
-import './page-header.scss';
+import "./page-header.scss";
 
-import React from 'react';
-import { string } from 'prop-types';
+import React from "react";
+import { string } from "prop-types";
 
 const PageHeader = ({ title, subtitle }) => (
   <div className="page-header">
     <h1 className="page-header-title">{title}</h1>
-    {subtitle
-      ? <h3 className="page-header-subtitle">{subtitle}</h3>
-      : null}
+    {subtitle ? <h3 className="page-header-subtitle">{subtitle}</h3> : null}
   </div>
 );
 
 PageHeader.defaultProps = {
-  subtitle: '',
+  subtitle: "",
 };
 
 PageHeader.propTypes = {
--- client/src/components/common/slider/slide.jsx
+++ client/src/components/common/slider/slide.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { string } from 'prop-types';
+import React from "react";
+import Flip from "react-reveal/Flip";
+import { string } from "prop-types";
 
 const Slide = ({ title, img, text }) => (
   <>
     <img src={img} alt="slide-img" />
     <div className="header-slider-center">
-      <h1>{title}</h1>
-      <p className="header-slider-text">{text}</p>
+      <Flip top cascade>
+        <h1>{title}</h1>
+        <p className="header-slider-text">{text}</p>
+      </Flip>
     </div>
   </>
 );
 
 Slide.defaultProps = {
-  title: 'VehRecovery',
-  text: 'Това е платформа, която помага на всички хора нуждаещи се от пътна помощ или различни транспортни услуги. Тук можете да откриете най-добрите и надеждни доставчици на услугите от които се нуждаете.',
+  title: "Jiva Recovery",
+  text:
+    "Това е платформа, която помага на всички хора нуждаещи се от пътна помощ или различни транспортни услуги. ",
 };
 
 Slide.propTypes = {
