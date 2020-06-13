58d28cd047845a34b8703cd59fd678af55bac1e6
Antonia Atanasova <antoniyaata@gmail.com>
2020-06-13 14:44:53 +0300

    Add request panel in profile administration

--- client/src/components/profile/request-panel.jsx
+++ client/src/components/profile/request-panel.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from "react";
+
+import Request from 'components/administration/request/request';
+import { AuthContext } from "../../ContextWrapper";
+import requestService from "../../services/request-service";
 
 const RequestPanel = () => {
-    return (
-        <article className='profile-request-panel'>
+  const { auth } = useContext(AuthContext);
+  let [requests, setRequests] = useState([]);
+  const userRequests = requests.filter(req => req.userId === auth);
 
-        </article>
-    )
-}
+  useEffect(() => {
+    requestService.getRequest("all").then((res) => {
+      setRequests(res);
+    });
+  }, []);
+
+  return (
+    <article className="profile-request-panel">
+      {userRequests.map((data) => (
+        <Request {...data}/>
+      ))}
+    </article>
+  );
+};
 
 export default RequestPanel;
