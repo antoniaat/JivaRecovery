576c3a8328ae949a6eec529c2e279cc0c51b60f9
Antonia Atanasova <antoniyaata@gmail.com>
2020-06-14 14:13:28 +0300

    Add request form wrapper in request page

--- client/src/components/forms/request/request-form.jsx
+++ client/src/components/forms/request/request-form.jsx
@@ -6,10 +6,12 @@ import {
   NotificationContainer,
   NotificationManager,
 } from "react-notifications";
+
+import userService from "services/user-service";
+import PageContentWrapper from "components/common/page-content-wrapper/page-content-wrapper";
 import requestService from "../../../services/request-service";
 import { AuthContext } from "../../../ContextWrapper";
 import FormHeader from "../form-header/form-header";
-import userService from "services/user-service";
 
 const RequestForm = () => {
   const [brand, setBrand] = useState("");
@@ -45,105 +47,105 @@ const RequestForm = () => {
         userService.edit(auth, userWithRequest);
       });
 
-      return (
-        NotificationManager.success(
-          "Заявката Ви беше успешно изпратена и ще бъде разгледана от администратор!",
-          "Поздравления!",
-          9000
-        )
-      )
+      return NotificationManager.success(
+        "Заявката Ви беше успешно изпратена и ще бъде разгледана от администратор!",
+        "Поздравления!",
+        9000
+      );
     });
   }
 
   return (
-    <div className="request-form">
-      <NotificationContainer />
-      <FormHeader
-        title="Нова заявка"
-        subtitle="Изпратете заявка за превоз на Вашето превозно средство"
-      />
-      <form onSubmit={handleFormSubmit}>
-        <label>
-          <input
-            type="text"
-            name="brand"
-            onChange={(ev) => setBrand(ev.target.value)}
-            value={brand}
-            placeholder="Марка"
-            required
-          />
-        </label>
-        <label>
-          <input
-            onChange={(ev) => setModel(ev.target.value)}
-            value={model}
-            type="text"
-            name="model"
-            placeholder="Модел"
-            required
-          />
-        </label>
-        <label>
-          <input
-            onChange={(ev) => setYear(ev.target.value)}
-            value={year}
-            type="number"
-            name="year"
-            placeholder="Година на производство"
-            required
-          />
-        </label>
-        <label>
-          <input
-            type="text"
-            name="pickupLocation"
-            onChange={(ev) => setPickupLocation(ev.target.value)}
-            value={pickupLocation}
-            placeholder="Местоположение за взимане"
-            required
-          />
-        </label>
-        <label>
-          <input
-            type="text"
-            name="deliveryLocation"
-            onChange={(ev) => setDeliveryLocation(ev.target.value)}
-            value={deliveryLocation}
-            placeholder="Местоположение за доставяне"
-            required
-          />
-        </label>
-        <label>
-          <input
-            type="text"
-            name="pickupDate"
-            onChange={(ev) => setPickupDate(ev.target.value)}
-            value={pickupDate}
-            placeholder="Дата за взимане"
-          />
-        </label>
-        <label>
-          <input
-            type="text"
-            name="deliveryDate"
-            onChange={(ev) => setDeliveryDate(ev.target.value)}
-            value={deliveryDate}
-            placeholder="Дата за доставяне"
-          />
-        </label>
-        <label>
-          <input
-            type="text"
-            name="condition"
-            onChange={(ev) => setCondition(ev.target.value)}
-            value={condition}
-            placeholder="Състояние на МПС"
-            required
-          />
-        </label>
-        <input type="submit" value="ИЗПРАТИ" />
-      </form>
-    </div>
+    <PageContentWrapper className='request-form-wrapper'>
+      <div className="request-form">
+        <NotificationContainer />
+        <FormHeader
+          title="Нова заявка"
+          subtitle="Изпратете заявка за превоз на Вашето превозно средство"
+        />
+        <form onSubmit={handleFormSubmit}>
+          <label>
+            <input
+              type="text"
+              name="brand"
+              onChange={(ev) => setBrand(ev.target.value)}
+              value={brand}
+              placeholder="Марка"
+              required
+            />
+          </label>
+          <label>
+            <input
+              onChange={(ev) => setModel(ev.target.value)}
+              value={model}
+              type="text"
+              name="model"
+              placeholder="Модел"
+              required
+            />
+          </label>
+          <label>
+            <input
+              onChange={(ev) => setYear(ev.target.value)}
+              value={year}
+              type="number"
+              name="year"
+              placeholder="Година на производство"
+              required
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              name="pickupLocation"
+              onChange={(ev) => setPickupLocation(ev.target.value)}
+              value={pickupLocation}
+              placeholder="Местоположение за взимане"
+              required
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              name="deliveryLocation"
+              onChange={(ev) => setDeliveryLocation(ev.target.value)}
+              value={deliveryLocation}
+              placeholder="Местоположение за доставяне"
+              required
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              name="pickupDate"
+              onChange={(ev) => setPickupDate(ev.target.value)}
+              value={pickupDate}
+              placeholder="Дата за взимане"
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              name="deliveryDate"
+              onChange={(ev) => setDeliveryDate(ev.target.value)}
+              value={deliveryDate}
+              placeholder="Дата за доставяне"
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              name="condition"
+              onChange={(ev) => setCondition(ev.target.value)}
+              value={condition}
+              placeholder="Състояние на МПС"
+              required
+            />
+          </label>
+          <input type="submit" value="ИЗПРАТИ" />
+        </form>
+      </div>
+    </PageContentWrapper>
   );
 };
 
