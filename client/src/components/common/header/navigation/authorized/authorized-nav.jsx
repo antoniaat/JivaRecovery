1c190bd51efeca6a6b7fc40b10c3c4ea58d77e01
Antonia Atanasova <antoniyaata@gmail.com>
2020-07-07 21:02:33 +0300

    Revert "Update authorized navbar"
    This reverts commit 45eec3cb16f978305aadbecd2193f6194c54828b.

--- client/src/components/common/header/navigation/authorized/authorized-nav.jsx
+++ client/src/components/common/header/navigation/authorized/authorized-nav.jsx
@@ -3,7 +3,7 @@ import ListItem from "../list-item/list-item";
 import userService from "../../../../../services/user-service";
 import { AuthContext } from "../../../../../ContextWrapper";
 
-const AuthorizedNav = ({ history }) => {
+const AuthorizedNav = () => {
   const { auth, setAuth } = useContext(AuthContext);
   const [isAdmin, setIsAdmin] = useState(false);
 
@@ -19,21 +19,16 @@ const AuthorizedNav = ({ history }) => {
     }, []);
   });
 
-  const adminItems = <ListItem link="/administration" text="Администрация" />;
-
-  {
-    /* <ListItem link="/profile" text="Профил" /> */
-  }
-
-  const userItems = <ListItem link="/request" text="Нова заявка" />;
-
   return (
     <ul>
       <ListItem link="/home" text="Начало" />
       <ListItem link="/about" text="Информация" />
       <ListItem link="/services" text="Видове транспорт" />
-
-      {isAdmin ? { adminItems } : { userItems }}
+      <ListItem link="/profile" text="Профил" />
+      {isAdmin 
+        ? <ListItem link="/administration" text="Администрация" />
+        : <ListItem link="/request" text="Нова заявка" />
+      }
       <li className="list-item">
         <a href="#" onClick={logout}>
           ИЗХОД
